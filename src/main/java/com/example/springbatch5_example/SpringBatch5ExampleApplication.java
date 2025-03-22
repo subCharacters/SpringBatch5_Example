bddcae1ba2bff59fc4caaa3627f3d143c65774a7
subCharacters <wldns@naver.com>
2025-03-22 23:45:01 +0900

    initial commit

---
+++ src/main/java/com/example/springbatch5_example/SpringBatch5ExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatch5_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatch5ExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatch5ExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbatch5_example/SpringBatch5ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatch5_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatch5ExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
